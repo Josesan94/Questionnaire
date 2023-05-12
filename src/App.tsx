b13211897586d9656fc29a575bd968022c051190
DESKTOP-NS1P2R3\joses <josemaria@nubimetrics.com>
2023-05-12 19:15:22 -0300

    integrate components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+
+import { Container, Box } from '@mui/material';
+import QuestionnaireTable from './components/QuestionnaireTable';
+import PeopleList from './components/Peoplelist';
+import AssignQuestionnaireForm from './components/AssignQuestionnaireForm';
+import AssignedQuestionnaireTable from './components/AssignedQuestionnaire';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Box sx={{ marginBottom: 2 }}>
+        <QuestionnaireTable />
+      </Box>
+      <Box sx={{ marginBottom: 2 }}>
+        <PeopleList />
+      </Box>
+      <Box sx={{ marginBottom: 2 }}>
+        <AssignQuestionnaireForm />
+      </Box>
+      <Box sx={{ marginBottom: 2 }}>
+        <AssignedQuestionnaireTable />
+      </Box>
+    </Container>
+  );
 }
 
-export default App
+export default App;
