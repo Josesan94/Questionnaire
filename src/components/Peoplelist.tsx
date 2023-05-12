9b6cb29927cb0f74df0cec9e0236baa25f3ef08f
DESKTOP-NS1P2R3\joses <josemaria@nubimetrics.com>
2023-05-12 17:50:24 -0300

    create peopleList component

---
+++ src/components/Peoplelist.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
+interface Person {
+  name: string;
+  surname: string;
+  email: string;
+  whatsapp: string;
+}
+
+const PeopleList: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get('/people');
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Failed to fetch people', error);
+      }
+    };
+
+    fetchPeople();
+  }, []);
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Surname</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>WhatsApp</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {people.map((person, index) => (
+            <TableRow key={index}>
+              <TableCell>{person.name}</TableCell>
+              <TableCell>{person.surname}</TableCell>
+              <TableCell>{person.email}</TableCell>
+              <TableCell>{person.whatsapp}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default PeopleList;
